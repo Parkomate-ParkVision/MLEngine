14ab38c0cc3b8e854db998bbdeb10988a157c0b2
notshrirang <shrirangmahajan123@gmail.com>
2023-11-03 00:35:41 +0530

    fix: flake8 errors

--- detect.py
+++ detect.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 import cv2
 import json
 from PIL import Image
-import numpy as np
 
 cam_port = 0
+
 
 def take_picture():
     """
@@ -13,16 +13,11 @@ def take_picture():
     """
     cam = cv2.VideoCapture(cam_port)
     result, image = cam.read()
-    # if result:
-    #     cv2.imshow("feed", image)
-    #     # cv2.imwrite("feed.png", image)
-    #     cv2.waitKey(0)
-    # else:
-    #     print("No image detected. Please! try again")
     cam.release()
     image = Image.fromarray(image)
     image = image.tobytes()
     return image
+
 
 def detect_numberplate(image):
     """
@@ -31,32 +26,39 @@ def detect_numberplate(image):
     Returns:
         A list of dictionaries containing the result of the detection.
     """
-    model = YOLO(model='./models/best.pt')
+    model = YOLO(model='./models/yolov8-best.pt')
     results = model.predict(image, conf=0.5)
     json_results = results[0].tojson()
 
-    encoded_json_results = str(json_results).replace("\n",'').replace(" ",'')
+    encoded_json_results = str(json_results).replace("\n", '').replace(" ", '')
     encoded_json_results = json.loads(encoded_json_results)
     return encoded_json_results
+
 
 def get_cropped_images(image):
     """
     image: PIL.Image
 
     Returns:
-        A list of PIL.Image objects containing the cropped images of the detected number plates.
+        A list of PIL.Image objects containing the cropped images of the
+        detected number plates.
     """
-    model = YOLO(model='./models/best.pt')
+    model = YOLO(model='./models/yolov8-best.pt')
     results = model.predict(image, conf=0.5)
     results[0].save_crop("predictions")
     json_results = results[0].tojson()
 
-    encoded_json_results = str(json_results).replace("\n",'').replace(" ",'')
+    encoded_json_results = str(json_results).replace("\n", '').replace(" ", '')
     encoded_json_results = json.loads(encoded_json_results)
 
     cropped_images = []
     for pred in encoded_json_results:
-        x1, y1, x2, y2 = pred['box']['x1'], pred['box']['y1'], pred['box']['x2'], pred['box']['y2']
-        cropped_image = image.crop((x1, y1, x2, y2))
+        x1 = pred['box']['x1']
+        y1 = pred['box']['y1']
+        x2 = pred['box']['x2']
+        y2 = pred['box']['y2']
+        cropped_image = image.crop(
+            (x1, y1, x2, y2)
+        )
         cropped_images.append(cropped_image.tobytes())
     return cropped_images
